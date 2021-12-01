c227a4ecfb453c868bff6577d64085e33a1a3978
Michael Yiu <mwyiu@calpoly.edu>
2021-12-01 02:19:54 -0800

    Z motor task

---
+++ ME 507 Clawmachine/src/Z_Motor_Task.cpp
@@ -0,0 +1,48 @@
+/** @file Z_Motor_Task.cpp
+ *    This file contains code to test a class for using encoders on STM32's in
+ *    the Arduino environment.
+ *
+ *  @author Michael Yiu
+ *  @date   2021-Dec-01
+ */
+
+#include <Arduino.h>
+#include <PrintStream.h>
+#include <STM32FreeRTOS.h>
+
+#include "shares.h"
+#include "motor_driver.h"
+#include "Z_Motor_Task.h"
+
+extern Share<uint16_t> share_user_positionz;
+
+void task_Z_motor(void *p_params)
+{
+  // Pointers to timer/counters used; could be in a task function
+  Serial << "Initializing Z motor...";
+  // MotorDriver Z_motor(PB10, PC7, PB3);
+  Serial << "done." << endl;
+
+  delay(1500);
+  Z_motor.enable();
+
+  int8_t power = 100;
+
+  Z_motor.setduty(power); // turn on at max
+
+  for (;;)
+  {
+    delay(1000);
+    // if (Z_encoder.getCount() < share_user_positionx.get()) // calculate how long to leave motor on to reach location
+    // {
+    // }
+    // else if (Z_encoder.getCount() > share_user_positionx.get())
+    // {
+    //   Z_motor.setduty(-100);
+    // }
+    // else
+    // {
+    //   Z_motor.setduty(0);
+    // }
+  }
+}
---
+++ ME 507 Clawmachine/src/Z_Motor_Task.h
@@ -0,0 +1,13 @@
+/** @file Z_Motor_Task.h
+ *  This file contains code for a task which records data somewhat quickly.
+ * 
+ *  @author Michael Yiu
+ *  @date   2021-Dec-01
+ */
+
+#ifndef _MOTORZ_TASK_H_
+#define _MOTORZ_TASK_H_
+
+void task_z_motor (void* p_params);
+
+#endif // _MOTORZ_TASK_H_
