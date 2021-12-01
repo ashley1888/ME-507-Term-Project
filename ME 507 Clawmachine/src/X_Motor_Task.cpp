f016c4406649c409c554aba3b4d720d4ccf5f662
Michael Yiu <mwyiu@calpoly.edu>
2021-12-01 02:16:30 -0800

    trying to incorporate shares

--- ME 507 Clawmachine/src/X_Motor_Task.cpp
+++ ME 507 Clawmachine/src/X_Motor_Task.cpp
@@ -1,51 +1,61 @@
-/** @file motor1_task.cpp
+/** @file X_Motor_Task.cpp
  *    This file contains code to test a class for using encoders on STM32's in
  *    the Arduino environment.
- * 
  *
- *  @author JR Ridgely
- *  @date   2020-Nov-15 Original file, based on stuff from the Interwebs
-
+ *  @author Michael Yiu
+ *  @date   2021-Dec-01
  */
 
 #include <Arduino.h>
 #include <PrintStream.h>
 #include <STM32FreeRTOS.h>
 
-// #include "shares.h"
+#include "shares.h"
 #include "motor_driver.h"
 #include "encoder_counter.h"
 #include "X_Motor_Task.h"
 
-void task_x_motor (void* p_params)
+extern Share<uint16_t> share_encoder_positionx;
+extern Share<uint16_t> share_user_positionx;
+
+void task_x_motor(void *p_params)
 {
-    // Pointers to timer/counters used; could be in a task function
-    Serial << "Initializing X motor and encoder...";
-    MotorDriver X_motor(PA0, PA1, PB3);
-    STM32Encoder X_encoder(TIM3, PB4, PB5);
-    Serial << "done." << endl;
+  // Pointers to timer/counters used; could be in a task function
+  Serial << "Initializing X motor and encoder...";
+  MotorDriver X_motor(PA0, PA1, PB3);
+  STM32Encoder X_encoder(TIM3, PB4, PB5);
+  Serial << "done." << endl;
 
-    delay(1500);
-    X_motor.enable();
+  // delay(1500);
+  X_motor.enable();
 
-    int8_t power = 100;
-    
-    X_motor.setduty(power); //turn on at max
+  // int8_t power = 100;
 
-    for (;;){
-        delay (500);
+  // X_motor.setduty(power);
+  X_motor.setduty(100); // turn on at max
 
-        Serial << "My X position is: " << X_encoder.getCount() << endl;        
-
-        if (X_encoder.getCount() >= 5000){ // spin til encoder 65,535
-
-          X_motor.setduty(-power);
-          // X_motor.disable();
-        }
-        if (X_encoder.getCount() <= 500){ // spin til encoder 65,535
-
-          X_motor.setduty(power);
-          // X_motor.disable();
-        }
+  for (;;)
+  {
+    delay(1000);
+    Serial << "My X position is: " << X_encoder.getCount() << endl;
+    // if (X_encoder.getCount() >= 5000) // spin til encoder 65,535
+    // {
+    //   X_motor.setduty(-power); // X_motor.disable();
+    // }
+    // if (X_encoder.getCount() <= 500) // spin til encoder 65,535
+    // {
+    //   X_motor.setduty(power); // X_motor.disable();
+    // }
+    if (X_encoder.getCount() < share_user_positionx.get())
+    {
     }
+    else if (X_encoder.getCount() > share_user_positionx.get())
+    {
+      X_motor.setduty(-100);
+    }
+    else
+    {
+      X_motor.setduty(0);
+    }
+  }
 }
--- ME 507 Clawmachine/src/Y_Motor_Task.cpp
+++ ME 507 Clawmachine/src/Y_Motor_Task.cpp
@@ -1,51 +1,61 @@
-/** @file motor1_task.cpp
+/** @file Y_Motor_Task.cpp
  *    This file contains code to test a class for using encoders on STM32's in
  *    the Arduino environment.
- * 
  *
- *  @author JR Ridgely
- *  @date   2020-Nov-15 Original file, based on stuff from the Interwebs
-
+ *  @author Michael Yiu
+ *  @date   2021-Dec-01
  */
 
 #include <Arduino.h>
 #include <PrintStream.h>
 #include <STM32FreeRTOS.h>
 
-// #include "shares.h"
+#include "shares.h"
 #include "motor_driver.h"
 #include "encoder_counter.h"
 #include "Y_Motor_Task.h"
 
-void task_y_motor (void* p_params)
+extern Share<uint16_t> share_encoder_positiony;
+extern Share<uint16_t> share_user_positiony;
+
+void task_y_motor(void *p_params)
 {
-    // Pointers to timer/counters used; could be in a task function
-    Serial << "Initializing Y motor and encoder...";
-    MotorDriver Y_motor(PB10, PC7, PB3);
-    STM32Encoder Y_encoder(TIM1, PA8, PA9);
-    Serial << "done." << endl;
+  // Pointers to timer/counters used; could be in a task function
+  Serial << "Initializing Y motor and encoder...";
+  MotorDriver Y_motor(PB10, PC7, PB3);
+  STM32Encoder Y_encoder(TIM1, PA8, PA9);
+  Serial << "done." << endl;
 
-    delay(1500);
-    Y_motor.enable();
+  delay(1500);
+  Y_motor.enable();
 
-    int8_t power = 100;
+  int8_t power = 100;
 
-    Y_motor.setduty(power); //turn on at max
+  Y_motor.setduty(power); // turn on at max
 
-    for (;;){
-        delay (500);
-
-        Serial << "My Y position is: " << Y_encoder.getCount() << endl;        
-
-        if (Y_encoder.getCount() >= 5000){ // spin til encoder 65,535
-
-          Y_motor.setduty(-power);
-          // Y_motor.disable();
-        }
-        if (Y_encoder.getCount() <= 500){ // spin til encoder 65,535
-
-          Y_motor.setduty(power);
-          // Y_motor.disable();
-        }
+  for (;;)
+  {
+    delay(1000);
+    Serial << "My Y position is: " << Y_encoder.getCount() << endl;
+    //   if (Y_encoder.getCount() >= 5000) // spin til encoder 65,535
+    //   {
+    //     Y_motor.setduty(-power); // Y_motor.disable();
+    //   }
+    //   if (Y_encoder.getCount() <= 500) // spin til encoder 65,535
+    //   {
+    //     Y_motor.setduty(power); // Y_motor.disable();
+    //   }
+    // }
+    if (Y_encoder.getCount() < share_user_positionx.get())
+    {
     }
+    else if (Y_encoder.getCount() > share_user_positionx.get())
+    {
+      Y_motor.setduty(-100);
+    }
+    else
+    {
+      Y_motor.setduty(0);
+    }
+  }
 }
