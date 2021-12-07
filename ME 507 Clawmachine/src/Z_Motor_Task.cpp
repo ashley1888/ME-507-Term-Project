c83a635ebd45eb8c7a41bc22d56c54890b8eb45b
Ashley Humpal <ahumpal@calpoly.edu>
2021-12-06 19:58:49 -0800

    z motor documented

--- ME 507 Clawmachine/src/Z_Motor_Task.cpp
+++ ME 507 Clawmachine/src/Z_Motor_Task.cpp
@@ -1,8 +1,8 @@
 /** @file Z_Motor_Task.cpp
- *    This file contains code to test a class for using encoders on STM32's in
- *    the Arduino environment.
+ *    This file contains the code for the motor operating in the z direction. It uses the 
+ *    motor driver to spin. 
  *
- *  @author Michael Yiu
+ *  @author Michael Yiu and Ashley Humpal
  *  @date   2021-Dec-01
  */
 
@@ -16,24 +16,23 @@
 
 void task_z_motor(void *p_params)
 {
-  // Pointers to timer/counters used; could be in a task function
   Serial << "Initializing Z motor...";
-  MotorDriver Z_motor(PB9, PB8, PB3, PA10);
+  MotorDriver Z_motor(PB9, PB8, PB3, PA10); // Creating motor object
   Serial << "done." << endl;
 
-  Z_motor.enable();
+  Z_motor.enable(); // Turn on motor
 
-  uint32_t time = 5000;
+  uint32_t time = 5000; // Variable representing duration 
 
   for (;;)
   {
-    queue_x_task.get();
-    queue_y_task.get();
+    queue_x_task.get(); // Gets x task value from queue 
+    queue_y_task.get(); // Gets y task value from queue 
     
-    Z_motor.setduty(-100);
-    delay(time);
-    queue_z_task.put(1);
-    share_z_job_status.put(1);
-    Z_motor.setduty(0);
+    Z_motor.setduty(-100); // Motor duty is set 
+    delay(time); // waits until time is over
+    queue_z_task.put(1); // puts 1 into queue to indicate z task complete
+    share_z_job_status.put(1); // puts 1 into share to indicate z task complete
+    Z_motor.setduty(0); // sets duty cycle to 0
   }
 }
