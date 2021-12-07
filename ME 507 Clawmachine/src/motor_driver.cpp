5857da2c8168bb974d4fc95179cb4817adc5845a
Ashley Humpal <ahumpal@calpoly.edu>
2021-12-06 19:21:53 -0800

    Motor driver documentation done

--- ME 507 Clawmachine/src/motor_driver.cpp
+++ ME 507 Clawmachine/src/motor_driver.cpp
@@ -1,60 +1,85 @@
 /** @file motor_driver.cpp
- *    This file 
+ *    This file contains code for a class that serves as a motor driver with methods to enable, disable, and
+ *    set the duty cycle of a motor. It also includes a method for fault detection.
  * 
- * 
+ *  @author Ashley Humpal and Michael Yiu 
+ *  @date   2021-Dec-06 
  */
 #include <Arduino.h>
 #include <PrintStream.h>
 #include "motor_driver.h"
 
-/** @brief   Cr
- * 
- *  @details This
- */
 
+/** @brief   Creates a constructor for the class that initializes variables for zero data.
+ *  @details This constructor takes various pins in as parameters.
+ *  @param   pin1 Pin representing the motor positive lead
+ *  @param   pin2 Pin representing the motor negative lead
+ *  @param   nsleep Pin representing the nsleep pin for power
+ *  @param   nfault Pin representing the motor nfault pin
+ */
 MotorDriver::MotorDriver (uint8_t pin1, uint8_t pin2, uint8_t nsleep, uint8_t nfault)
-//pin1, pin2, nsleep
-{  _nsleep = nsleep;
+{  _nsleep = nsleep; // Creating local variables
    _pin1 = pin1;
    _pin2 = pin2;
    _nfault = nfault;
    
-
-    pinMode(_pin1, OUTPUT); 
+    pinMode(_pin1, OUTPUT);  // Initializing pins
     pinMode(_pin2, OUTPUT); 
     pinMode(_nsleep, OUTPUT); 
     pinMode(nfault, INPUT_PULLUP); 
-    //pinMode(nfault, INPUT_PULLUP) -- MAKE FUNCT FOR WHEN LOGIC LOW IT KNOWS IT FAULT AND SETS MOTOR TO DISABLE
+    
 }
 
+
+/** @brief   Method to detect if motor has faulted.
+ *  @details Takes no parameters in and turns off motor if fault detected.
+ */
 void MotorDriver::faulted (void)
 {   
-    if (digitalRead(_nfault) == LOW) // if nfault pin is logic low 
+    if (digitalRead(_nfault) == LOW) // If nfault pin is logic low 
         { 
-         digitalWrite(_nsleep, LOW);  // disable motor
+         digitalWrite(_nsleep, LOW);  // Disable motor
         }
-    //attachInterrupt(digitalPinToInterrupt(nfault), blink, CHANGE); 
 }
-void MotorDriver::enable (void){   
-     digitalWrite(_nsleep, HIGH);
+
+
+/** @brief   Method to turn on motor.
+ *  @details Takes no parameters in and sets pin high.
+ */
+void MotorDriver::enable (void)
+{   
+     digitalWrite(_nsleep, HIGH); // Set pin high to turn on motor
 }
-void MotorDriver::disable (void){   
-    digitalWrite(_nsleep, LOW);
+
+
+/** @brief   Method to turn off motor.
+ *  @details Takes no parameters in and sets pin low.
+ */
+void MotorDriver::disable (void)
+{   
+    digitalWrite(_nsleep, LOW); // Set pin low to turn off motor
 }
-void MotorDriver::setduty (int8_t duty){   
+
+
+/** @brief   Method to set motor duty cycle.
+ *  @details Accounts for range of duty cycle values.
+ *  @param   duty  The duty cycle the motor is operating at
+ */
+void MotorDriver::setduty (int8_t duty)
+{   
     if (duty > 0){
-        digitalWrite(_pin1, HIGH);
+        digitalWrite(_pin1, HIGH); // motor runs counterclockwise
         digitalWrite(_pin2, LOW);
         analogWrite(_pin1, duty);
     }
     if (duty < 0){
-        digitalWrite(_pin1, LOW);
+        digitalWrite(_pin1, LOW); // motor runs clockwise
         digitalWrite(_pin2, HIGH);
         analogWrite(_pin1, -duty);
     }
     if(duty == 0)
     {
-        digitalWrite(_pin1, LOW);
+        digitalWrite(_pin1, LOW); // motor stops
         digitalWrite(_pin2, LOW);
         analogWrite(_pin1, duty);
     }
--- ME 507 Clawmachine/src/motor_driver.h
+++ ME 507 Clawmachine/src/motor_driver.h
@@ -1,30 +1,35 @@
 /** @file motor_driver.h
- *  This file 
+ *    This file contains the header for the motor driver class that has methods to enable, disable, and
+ *    set the duty cycle of a motor as well as fault detection.
  * 
- *  @author Michael Yiu
- *  @date   2021-Dec-01
+ *  @author Ashley Humpal and Michael Yiu 
+ *  @date   2021-Dec-06 
  */
 
 #include <Arduino.h>
 #include <PrintStream.h>
 
-/** @brief   Class
- *  @details This
+/** @brief   Class which implements a motor driver.
+ *  @details Contains methods to enable, disable, and set duty of motor.
  * 
  */
 class MotorDriver
 {
 protected:
-   // The number of points in the data set
-   uint32_t pinstatus;
+
+   /// Variable representing the nsleep pin for power
    uint8_t _nsleep;
+   /// Variable representing the nfault pin
    uint8_t _nfault;
+   /// Variable representing the the pin for the positive motor lead
    uint8_t _pin1;
+   // Variable representing the the pin for the negative motor lead
    uint8_t _pin2;
 public:
+   // Constructor which takes in multiple pins as parameters
    MotorDriver (uint8_t pin1, uint8_t pin2, uint8_t nsleep, uint8_t nfault );
-   void enable (void);
-   void faulted (void);
-   void disable (void);
-   void setduty (int8_t duty); // Constructor 
+   void enable (void); // turn on motor
+   void faulted (void); // fault detection
+   void disable (void); // disable motor 
+   void setduty (int8_t duty); // set motor duty 
 };
