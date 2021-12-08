74b35ae10eadc52567995647680bdc22d1167afa
Ashley Humpal <ahumpal@calpoly.edu>
2021-12-07 21:22:26 -0800

    mainpage doc

---
+++ ME 507 Clawmachine/src/mainpage.cpp
@@ -0,0 +1,12 @@
+/** @file mainpage.cpp
+ *    This website contains all relevant software information concerning our ME 507 Term project the Claw Machine.
+ * 
+ * 
+ *    At a high level what occurs in our project is that the controller task reads the user’s input position and sends that location to the x and y-motor tasks.
+ *    After reading the position, the two x and y-motors actuate and continue to rotate until the encoder indicates that they have reached the correct position, 
+ *    to a certain allowable tolerance. Once the correct x and y-positions have been reached, the z-motor activates for a predetermined length of time, descending the gripper. 
+ *    When the end effector reaches its destination, the gripper-motor actuates for a different predetermined length of time to close and grasps the object.
+ *  @author Ashley Humpal and Michael Yiu
+ *  @date 2021-01-Dec
+
+ */
